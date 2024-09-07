cee3b47cfc76b415f4cb26099a3cce8b439e1a70
Minerstove <rfgviterbo@gmail.com>
2024-09-07 16:12:23 +0800

    Finished Nokia Code

--- LectureNotes/lecture1FinalExercise.py
+++ LectureNotes/lecture1FinalExercise.py
@@ -1,6 +1,76 @@
 def nokiaCode_decorder():
-    nokiaCode = [6, 33, 7777, 7777, 2, 4, 33, 0, 444, 66, 8, 44, 33, 7, 666, 33, 6]
-
+    nokiaCode = ["6", "33", "7777", "7777", "2", "4", "33", "0", "444", "66", "8", "44", "33", "7", "666", "33", "6"]
+    letter_list = []
+    for code in nokiaCode:
+        match code[0]:
+            case "0":
+                if (len(code) == 1):
+                    letter_list.append(' ')
+            case "1":
+                if (len(code) == 1):
+                    letter_list.append('?')
+            case "2":
+                if (len(code) == 1):
+                    letter_list.append('a')
+                elif (len(code) == 2):
+                    letter_list.append('b')
+                elif (len(code) == 3):
+                    letter_list.append('c')
+            case "3":
+                if (len(code) == 1):
+                    letter_list.append('d')
+                elif (len(code) == 2):
+                    letter_list.append('e')
+                elif (len(code) == 3):
+                    letter_list.append('f')
+            case "4":
+                if (len(code) == 1):
+                    letter_list.append('g')
+                elif (len(code) == 2):
+                    letter_list.append('h')
+                elif (len(code) == 3):
+                    letter_list.append('i')
+            case "5":
+                if (len(code) == 1):
+                    letter_list.append('j')
+                elif (len(code) == 2):
+                    letter_list.append('k')
+                elif (len(code) == 3):
+                    letter_list.append('l')
+            case "6":
+                if (len(code) == 1):
+                    letter_list.append('m')
+                elif (len(code) == 2):
+                    letter_list.append('n')
+                elif (len(code) == 3):
+                    letter_list.append('o')
+            case "7":
+                if (len(code) == 1):
+                    letter_list.append('p')
+                elif (len(code) == 2):
+                    letter_list.append('q')
+                elif (len(code) == 3):
+                    letter_list.append('r')
+                elif (len(code) == 4):
+                    letter_list.append('s')
+            case "8":
+                if (len(code) == 1):
+                    letter_list.append('t')
+                elif (len(code) == 2):
+                    letter_list.append('u')
+                elif (len(code) == 3):
+                    letter_list.append('b')
+            case "9":
+                if (len(code) == 1):
+                    letter_list.append('w')
+                elif (len(code) == 2):
+                    letter_list.append('x')
+                elif (len(code) == 3):
+                    letter_list.append('y')
+                elif (len(code) == 4):
+                    letter_list.append('z')
+    final_word = "".join(letter_list)
+    return final_word
 
 def boardCode_decoder():
     original_string = '2Cm7zRRDo0+zetF#GVm0t9Metz!4a8dr9e&idt,sS2pDoa#3i05nm#'
@@ -38,4 +108,5 @@ def boardCode_decoder():
 
     return final_str
 
-print(boardCode_decoder)
+print(nokiaCode_decorder())
+print(boardCode_decoder())
